6ee0256e50a5c4a8354b3d304c097814e0783c8c
hosam5553 <hosam5553@gmail.com>
2024-02-14 07:56:59 +0000

    added new components

---
+++ src/components/Censored/Censored.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Censored() {
+  return <div></div>;
+}
+
+export default Censored;
---
+++ src/components/HitCounter/HitCounter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HitCounter() {
+  return <div></div>;
+}
+
+export default HitCounter;
